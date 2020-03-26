67917627ce72f9365c8b244ab5d97c31ef2701c9
denisha32 <43677295+denisha32@users.noreply.github.com>
2020-03-26 15:27:58 -0400

    Update insertUser procedure.sql
    returns created userID

--- Database/stored_procedures/insertUser procedure.sql
+++ Database/stored_procedures/insertUser procedure.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`administrator`@`localhost` PROCEDURE `insertUser`(in user_type enum('consumer', 'business', 'admin', 'both'), in email varchar(45), in password varchar(45), in dob Date, in first_name varchar(100), in last_name varchar(100))
+CREATE DEFINER=`administrator`@`localhost` PROCEDURE `insertUser`(in user_type enum('consumer', 'business', 'admin', 'both'), in email varchar(100), in password varchar(45), in dob Date, in first_name varchar(100), in last_name varchar(100), out new_user_id int(11))
 begin
 INSERT INTO users (`type`, email, `password`) values(user_type, email, `password`);
 set @uid = last_insert_id();
@@ -10,4 +10,6 @@ then
 Insert into business_users(user_id) values(@uid);
 end if;
 
+-- return newly created userid
+set new_user_id := last_insert_id();
 end
