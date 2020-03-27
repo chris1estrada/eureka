25f0750c6ba34bbda2eab4c645f97d0eadad40c5
denisha32 <43677295+denisha32@users.noreply.github.com>
2020-03-27 16:23:40 -0400

    Update insertDeal.sql

--- Database/stored_procedures/insertDeal.sql
+++ Database/stored_procedures/insertDeal.sql
@@ -1,16 +1,17 @@
 CREATE DEFINER=`administrator`@`localhost` PROCEDURE `insertDeal`(
-	IN BID INT(11), deal_id int(11), description varchar(1000), IN deal_type ENUM('Recurring', 'Limited'), 
+	IN BID INT(11), description varchar(1000), IN deal_type ENUM('Recurring', 'Limited'), 
     IN day_deal ENUM('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'), IN start_time TIME, IN end_time TIME,
     IN start_datetime DATETIME, end_datetime DATETIME)
 BEGIN
-    #deal_id not auto_incremented
-	INSERT INTO deals(deal_id, description) 
-    VALUES(deal_id, description);
+	INSERT INTO deals(description) 
+    VALUES(description);
     SET @DID = last_insert_id();
     
     INSERT INTO business_deals(business_id, deal_id)
     VALUES(BID, @DID);
     
+    #Fields are optional depending on the selection (limited/Recurring)
+    #for Limited option, null must be passed for deal_day 
     IF deal_type = 'Limited' THEN
 		INSERT INTO deal_hours(type, start_datetime, end_datetime, deal_id) 
 		VALUES('Limited', start_datetime, end_datetime, @DID);
