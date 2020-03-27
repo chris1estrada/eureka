b75dd740c962e9f103c6acb0d5f666ad3ebd2033
denisha32 <43677295+denisha32@users.noreply.github.com>
2020-03-27 16:12:44 -0400

    Update updateBusiness.sql

--- Database/stored_procedures/updateBusiness.sql
+++ Database/stored_procedures/updateBusiness.sql
@@ -1,45 +1,41 @@
 CREATE DEFINER=`administrator`@`localhost` PROCEDURE `updateBusiness`(
-	IN business_id INT(11), IN name VARCHAR(255), IN address VARCHAR(255), IN lat FLOAT, IN `long` FLOAT, 
-    IN menu VARCHAR(255), IN cuisine VARCHAR(45), IN description VARCHAR(1000), IN isAdult TINYINT(4))
+	IN BID INT(11), IN `name` VARCHAR(255), IN address VARCHAR(255), IN lat FLOAT, IN `long` FLOAT, 
+    IN menu VARCHAR(255), IN cuisine VARCHAR(45), IN `description` VARCHAR(1000), IN isAdult TINYINT(4))
 BEGIN
-	# Procedure that receives new data for an existing business and updates entry
-	# ---- (optional values) maybe pass nulls from client side ----
-	SELECT type INTO @type
-    FROM users
-    WHERE user_id = business_id;
-    
-    IF(@type = 'Business') THEN    
-		IF name IS NOT NULL THEN
-			UPDATE businesses b SET b.name = name 		
-			WHERE b.owner_id = business_id;
+	# Procedure that receives new data for an existing business and updates entrys that are not null
+    # For numeric types (lat, long and is Adult) the word 'null' needs to be passed from the client side
+    IF EXISTS (SELECT * FROM businesses b WHERE b.business_id = BID) THEN
+		IF `name` IS NOT NULL AND `name` != '' THEN
+			UPDATE businesses b SET b.`name` = `name` 		
+			WHERE b.business_id = BID;
 		END IF;
-        IF address IS NOT NULL THEN
+        IF address IS NOT NULL AND address != '' THEN
 			UPDATE businesses b SET	b.address = address 
-			WHERE b.owner_id = business_id;
+			WHERE b.business_id = BID;
 		END IF;
         IF lat IS NOT NULL THEN     
 			UPDATE businesses b SET b.lat = lat 		
-			WHERE b.owner_id = business_id;
+			WHERE b.business_id = BID;
 		END IF;
 		IF `long`IS NOT NULL THEN 
-			UPDATE businesses b SET b.long = `long`
-			WHERE b.owner_id = business_id;
+			UPDATE businesses b SET b.`long` = `long`
+			WHERE b.business_id = BID;
 		END IF;
-        IF menu IS NOT NULL THEN
+        IF menu IS NOT NULL AND menu != '' THEN
 			UPDATE businesses b SET b.menu = menu 		
-			WHERE b.owner_id = business_id;
+			WHERE b.business_id = BID;
 		END IF;
-        IF cuisine IS NOT NULL THEN   
+        IF cuisine IS NOT NULL AND cuisine != '' THEN   
 			UPDATE businesses b SET b.cuisine = cuisine 
-			WHERE b.owner_id = business_id;
+			WHERE b.business_id = BID;
 		END IF;
-        IF description IS NOT NULL THEN
-			UPDATE businesses b SET b.description = description 
-			WHERE b.owner_id = business_id;
+        IF `description` IS NOT NULL AND `description` != '' THEN
+			UPDATE businesses b SET b.`description` = `description` 
+			WHERE b.business_id = BID;
 		END IF;
 		IF isAdult IS NOT NULL THEN
 			UPDATE businesses b SET b.isAdult = isAdult 
-			WHERE b.owner_id = business_id; 
+			WHERE b.business_id = BID; 
 		END IF;
 	END IF;
 END
