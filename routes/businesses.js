5ca2cd4e5ab37dc692090623df8508e182ed3c25
anchet19 <anchet19@students.rowan.edu>
2020-04-01 19:33:38 -0400

    added mock data for front end to work with

--- routes/businesses.js
+++ routes/businesses.js
@@ -10,6 +10,29 @@ const db = require('../db')
 
 router.get('/', (request, response) => {
   // send back dummy data array of objects with data for homepage
+  response.json([
+    {
+      name: "Burger Barn",
+      image: 'https://senior-project-eureka.s3.amazonaws.com/97/photos/1585781577719.jpeg',
+      hasActiveDeals: false,
+      lat: 39.465591,
+      long: -75.006654
+    },
+    {
+      name: "Bagel University",
+      image: 'https://senior-project-eureka.s3.amazonaws.com/97/photos/1585781577719.jpeg',
+      hasActiveDeals: false,
+      lat: 39.464895,
+      long: -75.007373
+    },
+    {
+      name: "Magnolia",
+      image: 'https://senior-project-eureka.s3.amazonaws.com/97/photos/1585781577719.jpeg',
+      hasActiveDeals: true,
+      lat: 39.464912,
+      long: -75.006268
+    },
+  ])
 })
 
 router.get('/business_id', (request, response) => {
