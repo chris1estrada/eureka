80cd8e4e3b53c55e213fa1dbe3a28dc4f3bfcfdb
anchet19 <49288517+anchet19@users.noreply.github.com>
2020-04-02 13:53:42 -0400

    Update insertUser procedure.sql

--- Database/stored_procedures/insertUser procedure.sql
+++ Database/stored_procedures/insertUser procedure.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`administrator`@`localhost` PROCEDURE `insertUser`(in user_type enum('consumer', 'business', 'admin', 'both'), in email varchar(100), in password varchar(45), in dob Date, in first_name varchar(100), in last_name varchar(100), out new_user_id int(11))
+CREATE DEFINER=`administrator`@`localhost` PROCEDURE `insertUser`(in user_type enum('consumer', 'business', 'admin', 'both'), in email varchar(100), in password varchar(100), in dob Date, in first_name varchar(100), in last_name varchar(100), out new_user_id int(11))
 begin
 INSERT INTO users (`type`, email, `password`) values(user_type, email, `password`);
 set @uid = last_insert_id();
