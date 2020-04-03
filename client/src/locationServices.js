2eeede4a3a15f8a41f6741cca5bbb40e5c91f49e
Matthew Tam <tammat87@students.rowan.edu>
2020-04-03 12:58:11 -0400

    Using Promises to wait for locationServices.js to find lat/lng
    locationServices.js rewritten
    
    React hooks used to ensure that the latitude/longitude is updated appropriately.
    
    Thanks Chris Anchenta

--- client/src/locationServices.js
+++ client/src/locationServices.js
@@ -4,52 +4,14 @@ import { Component } from 'react';
 // This class definition is a React.Component so that we
 // can use it in multiple places for the app.
 
-export default class Location extends Component {
-
-    // The constructor takes properties defined as element attributes
-    // defined in JSX along with an initial default value for state.
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: [0, 0], // Null Island
-        error: null,
+const getLocation = new Promise((resolve, reject) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        console.log(latitude, longitude)
+        resolve({ lat: latitude, long: longitude })
       }
-    }
-  
-    // When the component is rendered to the DOM for the first time
-    // such as at page load we call the Geolocation API to determine
-    // a latitude and longitude for the browser
-  
-    componentDidMount() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            this.setState({
-              value: [position.coords.latitude, position.coords.longitude],
-              error: null,
-            });
-          },
-          (error) => this.setState(
-            {error: error.message}
-          )
-        );
-      }
-    }
-  
-  // Respond to user input with event callback
-    
-  changeLocation(evt) {
-      this.setState({
-          value: evt.target.value,
-        }
-      )
-    }
-  
-    // The JSX definition for how to render this component on the page.  
-    // This render function returns an array formatted like this:
-    // [latitude, longitude]
-    render() {
-      return this.state.value;
-      }
+    )
   }
+})
+export default getLocation
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,17 +1,28 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Location from '../../locationServices.js'
+import getLocation from '../../locationServices.js'
 
 // Homepage Variables
-const [lat, lng] = [<Location />]
-
 const HomePage = () => { 
-  const [] = useState ([])
-  return (
+
+  // Getting Location
+  const [state, setState] = useState({
+    //Null Island
+    lat: 0,
+    long: 0
+  })
+
+  useEffect(() => {
+    getLocation.then((result) => {
+      setState({ lat: result.lat, long: result.long })
+    })
+    console.log(state.lat + "____" + state.long);
+  }, [])
+
+
+  return (  
     <div>
-      {lat}
-      {lng}
+      {state.long}
       <h1>HOME PAGE</h1>
       <Link to={`/details/1`}>Business 1</Link>
       <Link to={`/details/2`}>Business 2</Link>
