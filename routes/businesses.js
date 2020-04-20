23a9d9b9440fecb935cd12ded9333be078bcc199
anchet19 <anchet19@students.rowan.edu>
2020-04-19 20:50:46 -0400

    converted queries to rusable functions

--- routes/businesses.js
+++ routes/businesses.js
@@ -1,20 +1,17 @@
 /**
- * 
- * 
  * @module Route callbacks for consumer-user facing business info
- * @author Jaxon Terrell
+ * @author Jaxon Terrell, Chris Ancheta
  */
+
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
 const path = require('path');
-//stored the SQL procs in an array for modularity if any procedures need to be added later
-let sqlArray = ['call selectBusiness(?);', 'call selectBusinessImages(?);', 'call selectBusinessHours(?);', 'call selectDeals(?);', 'call selectDealHours(?);']
+
+const { getBusinessDetails } = require('./controllers/business.controllers')
 
 /**
- * Retrieve a list of businesses containing:
- * business name, address, lat, long, cuisine, open/closed status, thumbnail image, and distance from user
- * @param request HTTP request body requires radius, lat, and long. May also provide 2 optional tag filters
+ * Retrieve a list of businesses containing 
  */
 router.get('/', (request, response) => {
   // destructure request body that way values will be null if not received
@@ -27,25 +24,13 @@ router.get('/', (request, response) => {
   })
 })
 
-router.get('/:business_id', (request, response) => {
-  //send back info for a particular business based on their unique business id
-  var jsonArray = [];
-  for (let step = 0; step <= sqlArray.length - 1; step++) {
-    db.query(sqlArray[step], request.params.business_id, (error, [[results]]) => {
-      if (error) {
-        return console.error(error.message);
-      }
-      //makes sure we only get a response once we've executed the last SQL proc 
-      if (step == (sqlArray.length - 1)) {
-        jsonArray.push(results);
-        response.json(jsonArray);
-        console.log(jsonArray);
-      }
-      else {
-        jsonArray.push(results);
-      }
-    });
-  };
+/**
+ * Retrieve all details for a given business
+ */
+router.get('/:business_id', async (request, response) => {
+  const { business_id } = request.params
+  const result = await getBusinessDetails(business_id)
+  response.json(result)
 });
 
 module.exports = router
