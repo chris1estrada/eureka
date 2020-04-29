ef62d87436fb7bf8492f324cbee677091eb2d368
anchet19 <anchet19@students.rowan.edu>
2020-04-29 17:59:22 -0400

    connect to auth context

--- client/src/pages/account/account.component.jsx
+++ client/src/pages/account/account.component.jsx
@@ -2,21 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
+import { useAuth } from '../../hooks/useAuth'
+
 const AccountPage = () => {
-  const { display_name } = useParams();
+  const { user } = useAuth()
+  const { bid } = useParams();
   const [connected, setConnected] = useState(false)
   useEffect(() => {
     const fetchAccount = async () => {
-      const response = await axios.get(`/api/v1/accounts/${display_name}`);
+      const response = await axios.get(`/api/v1/accounts/businesses/${bid}`);
       setConnected(response.data);
     }
 
-    fetchAccount();
-  }, [display_name])
+    // fetchAccount();
+  }, [bid])
 
   return (
     <div>
-      <h1>ACCOUNT PAGE {display_name}</h1>
+      <h1>ACCOUNT PAGE {bid}</h1>
       <p>{connected}</p>
     </div>
   );
