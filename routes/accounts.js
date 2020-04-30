6aba699162b3262de7a8ac24e57dec4ebea92c62
Jaxon Terrell <terrellj8@students.rowan.edu>
2020-04-30 18:02:44 -0400

    Update accounts.js

--- routes/accounts.js
+++ routes/accounts.js
@@ -204,42 +204,120 @@ router.get('/users/:user_id', checkToken, (request, response) => {
 });
 
 
-router.put('/businesses/:business_id', [ 
+router.put(
+  '/businesses/:business_id', 
+  (request, response, next) => {
+  /**
+   * middleware to handle multipart-form and file uploads
+   * uploads files to AWS S3 and separates request form into
+   * request.body and request.files
+   */
+    upload(request, response, (err) => {
+      if (request.fileValidationError) {
+        return response.send(request.fileValidationError);
+      }
+      else if (err instanceof multer.MulterError) {
+        return response.send(err.message);
+      }
+      else if (err) {
+        return response.send(err);
+      }
+      else {
+        next()
+      }
+    })
+  },
+  [ 
   //may need to add a param for business_id, depending on if we ask for user to input
-  body('name'), 
-  body('address'), 
-  body('lat'),
-  body('long'),
-  body('menu'),
-  body('cuisine'),
+  body(['uid', 'isAdult'], 'Field required').notEmpty().toInt(),
+  body('name', 'Field required').not().isEmpty(), 
+  body('tel')
+    .not()
+    .isEmpty()
+    .trim()
+    .matches(/^[2-9]\d{2}-\d{3}-\d{4}$/) // phone number must be in the form xxx-xxx-xxxx
+    .withMessage('Telephone number not a valid format'),
+  body(['address', 'cuisine'], 'Field required')
+    .not()
+    .isEmpty(),
+  body(['lat', 'lng'], 'Field required').notEmpty().toFloat(),
+  body('menu').not().isEmpty().trim(),
   body('description'),
-  body('isAdult'), //must be an int of 0 or 1
-  body('phoneNumber') //must be 10 or 11 digits
-],  async (request, response) => {
-  const errors = validationResult(request);
-  if (!errors.isEmpty()) {
-    return response.status(422).json({ errors: errors.array() })
-  }
-  // update info for a particular business based on their unique business id
-  const sql = 'CALL updateBusiness(?,?,?,?,?,?,?,?,?,?)'
-  db.query(
-    sql,
-    [
-      request.params.business_id,
-      request.body.name,
-      request.body.address,
-      request.body.lat,
-      request.body.long,
-      request.body.menu,
-      request.body.cuisine,
-      request.body.description,
-      request.body.isAdult,
-      request.body.phoneNumber
-    ],
-    (err, results) => {
-      if(err) response.status(422).json({ error: err.essage })
-      return response.status(200).json(results)
-    }) 
+  body('isAdult').toBoolean(), //must be an int of 0 or 1
+  ], 
+  (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(422).json({ errors: errors.array() })
+    }
+    try {
+      const { uid, name, address, lat, lng, cuisine, description, isAdult, tel } = request.body
+      const sql1 = 'CALL selectUser(?)'
+      db.query(sql1, [request.body.uid], (err, [[user]]) =>   {
+        if (err) return response.json({ error: err.message });
+        const { email, password } = user;
+        const menu = request.files.menu[0].location;
+        const photos = request.files.photo;
+        // update info for a particular business based on their unique business id
+        const sql2 = 'CALL updateBusiness(?,?,?,?,?,?,?,?,?,?)';
+        db.query(
+          sql2,
+          [
+            request.params.business_id,
+            name,
+            address,
+            lat,
+            lng,
+            menu,
+            cuisine,
+            description,
+            isAdult,
+            tel
+          ], 
+          async (err, results) => {
+            if(err) response.status(422).json({ error: err.essage })
+            const  BID  = results
+            if (photos) {
+              const sql3 = 'CALL insertImage(?,?,?)'
+              photos.forEach((photo) => {
+                db.query(sql3, [BID, photo.originalname, photo.location], (err, results) => {
+                  if (err) return response.json({ error: err })
+                })
+              })
+            }
+            // If the user provided any deals, insert them into the database
+            if (request.body.deals) {
+              const deals = await JSON.parse(request.body.deals)
+              const sql4 = 'CALL insertDeal(?,?,?,?,?,?,?,?)'
+              deals.forEach(({ description, type, day, start_time, end_time, start_datetime, end_datetime }) => {
+                db.query(sql4, [
+                  BID,
+                  description,
+                  type, day,
+                  start_time,
+                  end_time,
+                  start_datetime,
+                  end_datetime
+                ],
+                  (err, results) => {
+                    if (err) return response.json({ error: err })
+                  })
+              })
+            }
+            // Insert business hours of operation into the database
+            //const hours = await JSON.parse(request.body.hours)
+            //const sql5 = 'CALL insertBusinessHours(?,?,?,?)'
+            //hours.forEach(({ day, open_time, close_time }) => {
+            //  db.query(sql5, [BID, day, open_time, close_time], (err, results) => {
+            //    if (err) return response.json({ error: err })
+            //  })
+            //})
+            response.status(200).json('Business Updated')
+            })
+          })
+        } catch (err) {
+        response.status(422).json({ error: err })
+        }
 })
 
 router.put('/users/:user_id', checkToken, checkToken, (request, response) => {
