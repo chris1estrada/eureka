6af8a285000e49f3244e0b50dc4e56d2f7f24e22
anchet19 <anchet19@students.rowan.edu>
2020-04-30 03:08:20 -0400

    improved style for mobile view

--- client/src/components/day-event-list.js
+++ client/src/components/day-event-list.js
@@ -1,7 +1,8 @@
 import React, { Fragment, useState } from 'react'
 import moment from 'moment'
 import AddIcon from '@material-ui/icons/Add'
-import RemoveIcon from '@material-ui/icons/Remove'
+import DeleteIcon from '@material-ui/icons/Delete'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { KeyboardDateTimePicker, KeyboardTimePicker } from '@material-ui/pickers'
 import {
   Divider,
@@ -12,10 +13,10 @@ import {
   ListItemText,
   IconButton,
   MenuItem,
-  TextField,
   InputLabel,
   FormControl,
-  FormGroup
+  FormGroup,
+  TextareaAutosize,
 } from '@material-ui/core';
 
 /**
@@ -30,18 +31,27 @@ import {
  * @prop {callback} onRemove - A callback function that accepts the transformed list
  * @prop {boolean} [dateTime=false] - Used for providing a DateTime range
  */
-const DayEventList = (props) => {
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  const [selectedDay, setSelectedDay] = useState('Monday')
-  const [start, setStart] = useState(new Date())
-  const [end, setEnd] = useState(new Date())
-  const [description, setDescription] = useState()
-  const [errors, setErrors] = useState({})
+const DayEventList = ({ onAdd, onRemove, dateTime, ...props }) => {
+  const isDateTime = dateTime === "true" ? true : false
+  let STORAGE_FORMAT = ''
+  if (isDateTime) {
+    STORAGE_FORMAT = 'YYYY-MM-DD HH:mm'
+  } else {
+    STORAGE_FORMAT = 'HH:mm'
+  }
+  const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
+  const [selectedDay, setSelectedDay] = useState('Monday')
+  const [start, setStart] = useState(new moment())
+  const [end, setEnd] = useState(new moment())
+  const [description, setDescription] = useState("")
+  const [errors, setErrors] = useState({})
+  console.log(props.dateTime);
   const Picker = ({ label, value, onChange }) => (
-    <Fragment>
+    <div style={{ display: 'grid' }}>
       {
-        props.dateTime ?
+
+        isDateTime ?
           <KeyboardDateTimePicker
             label={label}
             helperText={errors.timeError}
@@ -63,23 +73,30 @@ const DayEventList = (props) => {
             onChange={onChange}
           />
       }
-    </Fragment>
+    </div>
   )
 
+  const setDefaultState = () => {
+    const now = new moment()
+    setSelectedDay('Monday')
+    setStart(now)
+    setEnd(now)
+    setDescription("")
+  }
   const onAddItem = () => {
-
+    console.log(moment(start).format(STORAGE_FORMAT))
     const startTime = moment(start).day()
     const endTime = moment(end).day()
     if (startTime > endTime) {
       setErrors({ timeError: 'End date cannot be before start date' })
     } else {
       setErrors({})
-      props.onAdd([
+      onAdd([
         ...props.items,
         {
-          day: selectedDay,
-          starts: moment(start).format('HH:mm'),
-          ends: moment(end).format('HH:mm'),
+          day: isDateTime ? false : selectedDay,
+          starts: moment(start).format(STORAGE_FORMAT),
+          ends: moment(end).format(STORAGE_FORMAT),
           description: description
         }
       ])
@@ -87,7 +104,7 @@ const DayEventList = (props) => {
   }
   const onRemoveItem = (removeIndex) => () => {
     console.log(removeIndex);
-    props.onRemove(props.items.filter((_, index) => {
+    onRemove(props.items.filter((_, index) => {
       return index !== removeIndex
     }))
   }
@@ -96,30 +113,40 @@ const DayEventList = (props) => {
     <List {...props} >
       {
         props.items.map((item, index) => (
-          <ListItem style={{ border: '1px solid lightgrey', borderRadius: '5px' }}>
-            <ListItemText >{item.day}</ListItemText>
-            <ListItemText >{moment(item.starts, 'hh:mm').format('LT')}</ListItemText>
-            <ListItemText >{moment(item.ends, 'hh:mm').format('LT')}</ListItemText>
-            <ListItemText >{item.description}</ListItemText>
-            <ListItemIcon children={<IconButton onClick={onRemoveItem(index)}><RemoveIcon /></IconButton>} />
+          <ListItem key={index} style={{ display: 'grid', justifyContent: 'space-between', gridTemplateColumns: '1fr, 1fr, 1fr, 50px', gridTemplateAreas: '"dt1 dt2 dt3 btn" "nu desc desc btn"', border: '1px solid lightgrey', borderRadius: '5px' }}>
+            <ListItemText style={{ gridArea: 'dt1' }}>{item.day ? item.day : moment(item.starts, STORAGE_FORMAT).format('LL')}</ListItemText>
+            <ListItemText style={{ gridArea: 'dt2' }}>
+              {moment(item.starts, STORAGE_FORMAT).format('LT')}
+            </ListItemText>
+            <ListItemText style={{ gridArea: 'dt3' }}>
+              {moment(item.ends, STORAGE_FORMAT).format('LT')}
+            </ListItemText>
+            <ListItemText style={{ gridArea: 'desc' }} >{item.description}</ListItemText>
+            <ListItemIcon style={{ gridArea: 'btn' }}>
+              <IconButton onClick={onRemoveItem(index)}><DeleteIcon /></IconButton>
+            </ListItemIcon>
           </ListItem>
         ))
       }
-      < ListItem style={{ display: 'grid' }}>
-        <FormGroup style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', justifyContent: 'center' }}>
-          {
-            props.dateTime ? false :
-              <FormControl >
-                <InputLabel id='day-selector'>Day of week</InputLabel>
-                <Select labelId='day-selector' value={selectedDay} onChange={e => setSelectedDay(e.target.value)}>
-                  {days.map(day => (<MenuItem key={day} value={day} >{day}</MenuItem>))}
-                </Select>
-              </FormControl>
-          }
-          <Picker dateTime={props.dateTime} label='Start' value={start} onChange={time => setStart(time)} />
-          <Picker dateTime={props.dateTime} label='End' value={end} onChange={time => setEnd(time)} />
-        </FormGroup>
-        <TextField label='Description' value={description} onChange={e => setDescription(e.target.value)} />
+
+      < ListItem style={{ display: 'grid', width: '100%', gridTemplateColumns: '1fr' }}>
+        {
+          isDateTime ? false :
+            <FormControl >
+              <InputLabel id='day-selector'>Day of week</InputLabel>
+              <Select labelId='day-selector' value={selectedDay} onChange={e => setSelectedDay(e.target.value)}>
+                {DAYS.map(day => (<MenuItem key={day} value={day} >{day}</MenuItem>))}
+              </Select>
+            </FormControl>
+        }
+        <Picker label='Start' value={start} onChange={time => console.log(time)} />
+        <Picker label='End' value={end} onChange={time => setEnd(time)} />
+        {props.description &&
+          <Fragment>
+            <InputLabel id='description'>Description</InputLabel>
+            <TextareaAutosize rowsMin={3} rowsMax={3} label='Description' multiline="true" value={description} onChange={e => setDescription(e.target.value)} />
+          </Fragment>
+        }
         <ListItemIcon style={{ justifySelf: 'end' }} children={<IconButton onClick={onAddItem}><AddIcon /></IconButton>} />
       </ListItem >
       <Divider style={{ marginTop: '8px' }} />
