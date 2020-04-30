ff0bec15c62d56f957bfd1bcb14aa15af8904778
anchet19 <anchet19@students.rowan.edu>
2020-04-30 03:07:23 -0400

    remove unused import

--- client/src/pages/register/business-registration-page.jsx
+++ client/src/pages/register/business-registration-page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Container from '@material-ui/core/Container'
-import { CssBaseline } from '@material-ui/core';
 import BusinessForm from '../../components/business-form';
 
 const BusinessRegistrationPage = () => {
