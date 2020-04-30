babd0a9b225b9c9736904d1158a2f7153dd092bf
denisha32 <43677295+denisha32@users.noreply.github.com>
2020-04-30 18:11:21 -0400

    Update updateDealHours&Desc.sql

--- Database/stored_procedures/updateDealHours&Desc.sql
+++ Database/stored_procedures/updateDealHours&Desc.sql
@@ -16,41 +16,41 @@ BEGIN
 			IF start_datetime IS NOT NULL THEN
 				UPDATE deal_hours dh SET dh.start_datetime = start_datetime
 				WHERE dh.deal_id = deal_id;
-            END IF;
-            IF end_datetime IS NOT NULL THEN
+            		END IF;
+            		IF end_datetime IS NOT NULL THEN
 				UPDATE deal_hours dh SET dh.end_datetime = end_datetime
 				WHERE dh.deal_id = deal_id;
-            END IF;
+            		END IF;
             
-            #Remove info if we are changing the deal from recurring to limited
-            IF @dtype = 'Recurring' THEN
+            		#Remove info if we are changing the deal from recurring to limited
+            		IF @dtype = 'Recurring' THEN
 				UPDATE deal_hours dh SET dh.`type` = 'Limited', dh.weekday = null, dh.start_time = null, dh.end_time = null 
 				WHERE dh.deal_id = deal_id;
-            END IF;    
+            		END IF;    
            
 		ELSEIF deal_type = 'Recurring' THEN
-            IF weekday IS NOT NULL THEN
+            		IF weekday IS NOT NULL THEN
 				UPDATE deal_hours dh SET dh.weekday = weekday
 				WHERE dh.deal_id = deal_id;
-            END IF;
-             IF start_time IS NOT NULL THEN
+            		END IF;
+             		IF start_time IS NOT NULL THEN
 				UPDATE deal_hours dh SET dh.start_time = start_time
 				WHERE dh.deal_id = deal_id;
-            END IF;
-            IF end_time IS NOT NULL THEN
+            		END IF;
+            		IF end_time IS NOT NULL THEN
 				UPDATE deal_hours dh SET dh.end_time = end_time
 				WHERE dh.deal_id = deal_id;
-            END IF;
+            		END IF;
             
 			#Remove info if we are changing the deal from limited to recurring
-            IF @dtype = 'Limited' THEN
+            		IF @dtype = 'Limited' THEN
 				UPDATE deal_hours dh SET dh.`type` = 'Recurring', dh.start_datetime = null, dh.end_datetime = null
 				WHERE dh.deal_id = deal_id;
-            END IF;  
+           		 END IF;  
 		END IF;
         
 		# Update deal description 
-         IF `description` IS NOT NULL AND `description` != '' THEN
+         	IF `description` IS NOT NULL AND `description` != '' THEN
 			UPDATE deals d SET d.`description` = `description` 		
 			WHERE d.deal_id = deal_id; 
 		END IF;
