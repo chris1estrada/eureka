1f7c3bc94870a5a1a5fdb2227d1a88f81822364f
ryanhardin56 <61746631+ryanhardin56@users.noreply.github.com>
2020-04-30 21:15:14 -0400

    Update selectUser procedure.sql

--- Database/stored_procedures/selectUser procedure.sql
+++ Database/stored_procedures/selectUser procedure.sql
@@ -1,11 +1,10 @@
-  
-CREATE DEFINER=`administrator`@`localhost` PROCEDURE `selectUser`(in consumer_id smallint)
+CREATE DEFINER=`administrator`@`localhost` PROCEDURE `selectUser`(in consumer_id int)
 begin
 
 select type into @type -- puts result into the variable
 from users
 where user_id = consumer_id;
-if (@type = 'consumer')
+if (@type = 'both' or @type = 'consumer')
 then select * 
 from users join consumers using (user_id)
 where user_id = consumer_id;
